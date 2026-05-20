60444c36dd88bbea1edbe915b8428759f150dba6
ridhakurfi <ridhakurfi@gmail.com>
2026-05-20 12:51:11 +0700

    preparation for render deploy

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+
+  test: {
+    dialect: "postgres",
+  },
+
+  production: {
+    dialect: "postgres",
+  },
+};
